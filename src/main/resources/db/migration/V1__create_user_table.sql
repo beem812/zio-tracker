deae60ef3d14fca23a0d637dd13dc0972337c351
James Beem <beem132@gmail.com>
2021-07-18 15:59:35 -0400

    switch tracker user to a uuid

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,7 +1,10 @@
+--  CREATE EXTENSION IF NOT EXISTS "uuid-ossp"
+
 CREATE TABLE tracker_user(
-  id INT IDENTITY PRIMARY KEY,
-  name VARCHAR(255)
+  id VARCHAR(50) DEFAULT random_uuid() NOT NULL PRIMARY KEY,
+  email VARCHAR(255),
+  auth0_id VARCHAR(255)
 );
 
-INSERT INTO tracker_user (id, name)
-VALUES (1, 'james');
+INSERT INTO tracker_user (email, auth0_id)
+VALUES ('bleep@blop.com', 'anId');

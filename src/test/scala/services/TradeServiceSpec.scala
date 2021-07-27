518f447568d9be33c07bc2899ad42ad48fcd17b0
James Beem <beem132@gmail.com>
2021-07-26 20:56:08 -0400

    initial wip

--- src/test/scala/services/TradeServiceSpec.scala
+++ src/test/scala/services/TradeServiceSpec.scala
@@ -4,12 +4,21 @@ import zio.test.DefaultRunnableSpec
 import java.util.UUID
 import zio.test.Assertion._
 import zio.test._
+import zio.test.TestAspect._
 import zio.magic._
 import repos.TradeRepo
 import util.Db
 import aspects.DbAspects
+import dtos._
 
 object TradeServiceSpec extends DefaultRunnableSpec {
+  import Db.ctx._
+  val testData = for {
+    user  <- TrackerUser.genUser.runHead.get
+    trade <- Trade.genTradeWithUser(user.id).runHead.get
+    _     <- DbAspects.loadTestData(trade)
+  } yield trade
+  val loadDataAspect = before(testData)
   val tests = suite("TradeService")(
     testM("does something") {
       val result = TradeService(_.getTradesByUserId(UUID.randomUUID()))
